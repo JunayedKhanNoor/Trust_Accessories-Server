87f865dffd6594fd2287d65be1dfc8b7f80804c7
163002029.junayed <junayednoor@gmail.com>
2022-05-22 16:13:04 +0600

    Admin Validation from backend

--- index.js
+++ index.js
@@ -58,6 +58,12 @@ async function run() {
         });
         res.send({ result, token });
       });
+      app.get("/admin/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({ email: email });
+        const isAdmin = user.role === "admin";
+        res.send({ admin: isAdmin });
+      });
   } finally {
   }
 }
