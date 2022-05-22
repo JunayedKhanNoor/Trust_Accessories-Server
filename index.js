65ecc80fef11e2f328df9fe3c5d6ac8bc1c13848
163002029.junayed <junayednoor@gmail.com>
2022-05-22 14:50:37 +0600

    jwt verification

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ app.use(express.json());
 
 const uri = "mongodb+srv://trust_accessories:Mycz9sQGUp1n5VIv@cluster0.jmcvs.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: "UnAuthorizes access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 
 async function run() {
   try {
